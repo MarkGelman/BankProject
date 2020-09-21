007955e16fcd53fdbb70fb7e8ff1e640170ff37f
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2020-09-21 22:51:43 +0300

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProcecc
+{
+    public class Account
+    {
+        private static int _numberOfAcc = 0;
+        private readonly int _accountNumber;
+        private readonly Customer _accountOwner;
+        private int _maxMinusAllowed;
+
+        public int AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+        }
+
+        public int Balance { get; private set; }
+        public Customer AccountOwner
+        {
+            get
+            {
+                return _accountOwner;
+            }
+        }
+
+        public int MaxMinusAllowed
+        {
+            get
+            {
+                return _maxMinusAllowed;
+            };
+        }
+
+
+        public Account(Customer customer, int monthlyincome)
+        {
+            _accountOwner = customer;
+            Balance = monthlyincome;
+            _accountNumber = customer.CustomerID;
+            _maxMinusAllowed = monthlyincome*3;
+        }
+
+        public void Add (int amount)
+        {
+
+        }
+
+        public void Subtract(int amount)
+        {
+
+        }
+
+
+        public static bool operator ==(Account account1, Account account2)
+        {
+            if (account1 is null && account2 is null)
+                return true;
+            if (account2 is null || account1 is null)
+                return false;
+
+            return account1._accountNumber == account2._accountNumber;
+        }
+
+        public static bool operator !=(Account account1, Account account2)
+        {
+            return !(account1 == account2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this._accountNumber.Equals(obj as Account);
+        }
+
+        public override int GetHashCode()
+        {
+            return this._accountNumber;
+        }
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProcecc
+{
+    public class Customer
+    {
+        private static int   _numberOfCust = 0;
+        private readonly int _customerID;
+        private readonly int _customerNumber;
+
+        public string Name { get; private set; }
+        public int PhNumber { get; private set; }
+        public int CustomerNumber { get; }
+
+        public int CustomerID { get; }
+
+
+        public Customer(int id, string name,int phone)
+        {
+            _customerID = id;
+            Name = name;
+            PhNumber = phone;
+            CustomerID = ++_numberOfCust;
+        }
+        
+
+        public static bool operator == (Customer customer1,Customer customer2)
+        {
+            if (customer1 is null && customer2 is null)
+                return true;
+            if (customer2 is null || customer1 is null)
+                return false;
+
+            return customer1.CustomerID == customer2.CustomerID;
+        }
+
+        public static bool  operator !=(Customer customer1, Customer customer2)
+        {
+            return !(customer1 == customer2);
+        }
+
+        public override bool Equals (object obj)
+        {
+            return this.CustomerID.Equals(obj as Customer);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.CustomerID;
+        }
+    }
+}
